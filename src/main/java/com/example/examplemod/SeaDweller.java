c8cd7b602aed0a92510dcf0efe6782f06b61a246
phizal <phillips.andrew351@gmail.com>
2024-04-16 15:51:05 -0400

    Renamed mod file

--- src/main/java/com/example/examplemod/SeaDweller.java
+++ src/main/java/com/example/examplemod/SeaDweller.java
@@ -27,8 +27,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(SeaDweller.MODID)
+public class SeaDweller
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -44,7 +44,7 @@ public class ExampleMod
     // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
     public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
 
-    public ExampleMod()
+    public SeaDweller()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
